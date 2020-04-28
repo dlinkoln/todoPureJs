b07836f18bcfc6e4f3544be9c4b72b02f12f2477
dlinkoln <dlinkoln99@gmail.com>
2020-04-28 14:53:00 +0300

    add id func in localstorage

--- src/app.js
+++ src/app.js
@@ -3,24 +3,34 @@ Entry points [@Create],[@All,@Done,@undone],[@Asc,@Desc],[@searchInput],[@CloseB
 View points {{TaskAmountDone}},{{TaskAmount}},{{FE Task(i)}},{{ProgressBar}}
 
 //First stage [@Create],[@CloseBtn(i)]
-0.Pushing/Getting task from LocalStorage
-1.Creating task from Title,Priority,and push into LocalStorage
+0.Pushing/Getting task from LocalStorage +
+1.Creating task from Title,Priority,and push into LocalStorage +
 2.Deleting task from {{FE Task(i)}}
+    a)Push to LS id of item
+    b)Bind item id in data-atribute
+    c)After deleting of item rebuild id from 1 to n(?optional)
+
 3.Check task as completed
 //Second stage [@All,@Done,@undone]
 0.Creating mutable list,View from mutable list
 1.All output
 2.Done output,filter tasks from unmutable list to mutable
 3.Undone same as 2
-//Third stage [@searchInput]
+//Third stage [@Asc,@Desc]
+1.Asc filter
+2.Desc filter
+//Fourth stage [@searchInput]
 0.Creating live search
 */
 
 
+//ROOT DATA
 const createBtn = document.querySelector('.create__btn')
 const root = document.querySelector('#list_view')
 let tasks = getFromLS() || []
 
+
+//LS HANDLERS
 function pushToLS(tasks) {
     let readyForPush = JSON.stringify(tasks)
     localStorage.setItem("tasks", readyForPush)
@@ -28,12 +38,22 @@ function pushToLS(tasks) {
 function getFromLS() {
     return JSON.parse(localStorage.getItem("tasks"))
 }
+function idFunc() {
+    if (localStorage.getItem("tasks") == null) {
+        return 1
+    } else {
+        let prevTask = JSON.parse(localStorage.getItem("tasks"))
+        //Primary key for tasks based on previous task
+        return prevTask[prevTask.length - 1].id + 1;
+    }
+}
 
+//CREATION FUNC
 function createTask() {
     let content = document.querySelector('.create__title').value
-    // console.log(tasks)
     if (content !== '') {
         tasks.push({
+            id: idFunc(),
             content: document.querySelector('.create__title').value,
             completed: true,
             priority: document.querySelector('.create__priority').value,
@@ -42,7 +62,7 @@ function createTask() {
         pushToLS(tasks)
         tasks.forEach(task => {
             renderList(task, root)
-            console.log(task)
+            // console.log(task)
         })
     }
     else {
@@ -56,11 +76,15 @@ function renderList(task, root) {
             ${task.content}
         </p>
         <span class="list__priority">${task.priority}</span>
-        <i class="close">&times;</i>
+        <i class="close" data-close=${task.id}>&times;</i>
     </div>
     `
+
     root.insertAdjacentHTML('afterbegin', $task)
 }
+
+
+//EVENTS
 createBtn.onclick = function () {
     createTask();
 }
